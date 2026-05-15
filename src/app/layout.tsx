5820235a463583d468337fad84f287460e46b929
shubham <s89301510@gmail.com>
2026-05-15 21:52:28 +1400

    feat: initialize root layout with metadata and add sitemap.xml for SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,6 +31,9 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        <meta name="robots" content="index,follow"></meta>
+      </head>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
